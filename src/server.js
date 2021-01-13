3303cc6e9f2a906607975cc646bb8502d47f98a4
Henrique <Carlos Henrique>
2021-01-13 18:40:47 -0300

    modified:  src/server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
+const router = require('./routes')
 const app = express()
 
 app.use(express.json())
+app.use(router)
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 8080;
 
 app.listen(port, (err)=> {
     if(err) {
